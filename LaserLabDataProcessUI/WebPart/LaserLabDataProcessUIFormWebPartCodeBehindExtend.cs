0d4cb55e14546f78d4627704785389495014b4c1
Amazingbow <wodemanur@163.com>
2017-01-10 09:01:23 +0800

    BE 添加一个字段 UI代码修改

--- LaserLabDataProcessUI/WebPart/LaserLabDataProcessUIFormWebPartCodeBehindExtend.cs
+++ LaserLabDataProcessUI/WebPart/LaserLabDataProcessUIFormWebPartCodeBehindExtend.cs
@@ -25,6 +25,8 @@ using UFSoft.UBF.UI.WebControls.ClientCallBack;
 using System.IO;
 using UFIDA.U9.UI.PDHelper;
 using System.Web;
+using UFIDA.U9.Cust.XMQX.LaserLabBE.LaserLab;
+using System.Configuration;
 
 
 
@@ -37,29 +39,31 @@ namespace LaserLabDataProcessUIModel
     public partial class LaserLabDataProcessUIFormWebPart
     {
         #region Custome eventBind
-        private const string JYTXT = "~/LaserLabProcessFiles/STOCK1.txt";
-        private const string BZTXT = "~/LaserLabProcessFiles/STOCK2.txt";
-        private const string CHTXT = "~/LaserLabProcessFiles/STOCK3.txt";
-        private const string BFTXT = "~/LaserLabProcessFiles/SCRAP.txt";
-
+        private const string JYTXT = "STOCK1.txt";
+        private const string BZTXT = "STOCK2.txt";
+        private const string CHTXT = "STOCK3.txt";
+        private const string BFTXT = "SCRAP.txt";
+        private string _ProcessFilePath;
+        private string _ErrorFilePath;
         //BtnCheckFile_Click...
         private void BtnCheckFile_Click_Extend(object sender, EventArgs e)
         {
+            //config.Text
             StringBuilder sb = new StringBuilder();
             //调用模版提供的默认实现.--默认实现可能会调用相应的Action.
-            if (File.Exists(HttpContext.Current.Server.MapPath(JYTXT)))
+            if (File.Exists(_ProcessFilePath + JYTXT))
             {
                 sb.AppendLine("有金油数据可以转换！");
             }
-            if (File.Exists(HttpContext.Current.Server.MapPath(BZTXT)))
+            if (File.Exists(_ProcessFilePath + BZTXT))
             {
                 sb.AppendLine("有包装数据可以转换！");
             }
-            if (File.Exists(HttpContext.Current.Server.MapPath(CHTXT)))
+            if (File.Exists(_ProcessFilePath + CHTXT))
             {
                 sb.AppendLine("有出货数据可以转换！");
             }
-            if (File.Exists(HttpContext.Current.Server.MapPath(BFTXT)))
+            if (File.Exists(_ProcessFilePath + BFTXT))
             {
                 sb.AppendLine("有报废数据可以转换！");
             }
@@ -76,22 +80,84 @@ namespace LaserLabDataProcessUIModel
         private void BtnChange_Click_Extend(object sender, EventArgs e)
         {
             //调用模版提供的默认实现.--默认实现可能会调用相应的Action.
-            
-            StreamReader sr = new StreamReader("", Encoding.Default);
-            String line;
-            while ((line = sr.ReadLine()) != null)
+            switch (this.RadioBoxList1.SelectedIndex)
             {
-                Console.WriteLine(line.ToString());
+                case 0:
+                    GoldenOilProcess(_ProcessFilePath + JYTXT);
+                    break;
+                case 1:
+                    PackingProcess(_ProcessFilePath + BZTXT);
+                    break;
+                case 2:
+                    ShipmentProcess(_ProcessFilePath + CHTXT);
+                    break;
+                case 3:
+                    ScrapProcess(_ProcessFilePath + BFTXT);
+                    break;
+                default:
+                    break;
             }
-
             BtnChange_Click_DefaultImpl(sender, e);
         }
 
+        private void ScrapProcess(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShipmentProcess(string path)
+        {
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            string errFileName = _ErrorFilePath + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            String line;
+            bool flag = false;
+            StreamWriter sw = null;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //LaserLab
+                line = line.Trim();
+                if (line.Length > 20)
+                {
+                    string batch = line.Substring(0, 5);
+                    string tempstr = line.Substring(6).Trim();
+
+                    string tempTime = tempstr.Substring(tempstr.Length - 15).Trim();
+                    string time = tempTime.Substring(0, 8);
+                    string laserLab = tempstr.Replace(tempTime, "").Trim();
+                    LaserLab lab = LaserLab.Finder.Find("BN='" + batch + "' and LB='" + laserLab + "'");
+                    if (lab == null)
+                    {
+                        flag = true;
+                        if (File.Exists(errFileName) == false)//如果不存在就创建file文件夹
+                        {
+                            File.Create(errFileName);//创建该文件
+                        }
+                        sw = new StreamWriter(errFileName);
+                        sw.WriteLine(line, Encoding.Default);
+                    }
+                }
+            }
+            if (sw != null)
+            {
+                sw.Close();
+            }
+            sr.Close();
+        }
+
+        private void PackingProcess(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GoldenOilProcess(string path)
+        {
+            throw new NotImplementedException();
+        }
         //BtnJYForceChange_Click...
         private void BtnJYForceChange_Click_Extend(object sender, EventArgs e)
         {
             //调用模版提供的默认实现.--默认实现可能会调用相应的Action.
-             
+
 
             BtnJYForceChange_Click_DefaultImpl(sender, e);
         }
@@ -128,7 +194,8 @@ namespace LaserLabDataProcessUIModel
         #region 自己扩展 Extended Event handler
         public void AfterOnLoad()
         {
-
+            _ProcessFilePath = ConfigurationManager.AppSettings["ProcessFilePath"];
+            _ErrorFilePath = ConfigurationManager.AppSettings["ErrorFilePath"];
         }
 
         public void AfterCreateChildControls()
