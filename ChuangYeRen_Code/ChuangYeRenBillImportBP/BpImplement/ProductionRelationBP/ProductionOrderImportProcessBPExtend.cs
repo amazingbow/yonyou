2bd1c247aedc50d1c9f3887f3391335d2b221f64
Amazingbow <wodemanur@163.com>
2017-04-18 16:48:35 +0800

    提交dll

--- ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/ProductionRelationBP/ProductionOrderImportProcessBPExtend.cs
+++ ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/ProductionRelationBP/ProductionOrderImportProcessBPExtend.cs
@@ -7,6 +7,7 @@
     using System.Collections.Generic;
     using System.Text;
     using UFIDA.U9.Base;
+    using UFIDA.U9.CBO.Pub.Controller;
     using UFIDA.U9.ISV.MO;
     using UFIDA.U9.ISV.MO.Proxy;
     using UFSoft.UBF.AopFrame;
@@ -49,6 +50,22 @@
                 if (data.Count == 0) return pub;
                 pub.DocID = data[0].ID;
                 pub.DocNo = data[0].DocNo;
+                ApproveMoProxy approveProxy = new ApproveMoProxy();
+                approveProxy.MOOperateParamDTOs = new List<MOOperateParamDTOData>();
+                MOOperateParamDTOData appData = new MOOperateParamDTOData
+                {
+                    MODocNo = pub.DocNo,
+                    OperateBy = Context.LoginUser,
+                    OperateOn = DateTime.Now,
+                    OperateQty = moDto.MRPQty,
+                    Org = new CBO.Pub.Controller.CommonArchiveDataDTOData { ID = Context.LoginOrg.ID, Code = Context.LoginOrg.Code, Name = Context.LoginOrg.Name }
+                };
+                approveProxy.MOOperateParamDTOs.Add(appData);
+                var appResult = approveProxy.Do();
+                StartMoProxy startMoProxy = new StartMoProxy();
+                startMoProxy.MOOperateParamDTOs = new List<MOOperateParamDTOData>();
+                startMoProxy.MOOperateParamDTOs.Add(appData);
+                var startResult = startMoProxy.Do();
             }
             catch (Exception ex)
             {
@@ -58,31 +75,31 @@
             return pub;
         }
 
+
         private MODTOData GetMoInfo(long RelationId)
         {
             PMakReqOrder order = PMakReqOrder.Finder.FindByID(RelationId);
             MODTOData moDto = new MODTOData();
             moDto.ActualStartDate = order.ReceiveDate;//业务开始时间
-            moDto.AlternateType = 0;//生产目的
-            //moDto.BOMECONo//BOMECONo  BOM变更号 
-            moDto.BOMEffDate = DateTime.MaxValue;//BOMEffeDate  BOM有效日期  
-            moDto.BOMType = 0;//BOM类型，0是自制，默认自制
-            moDto.BOMVersion = new CBO.Pub.Controller.CommonArchiveDataDTOData();
-            moDto.BusinessPerson = new UFIDA.U9.CBO.Pub.Controller.CommonArchiveDataDTOData
+            //moDto.AlternateType = 0;//生产目的
+            ////moDto.BOMECONo//BOMECONo  BOM变更号 
+            //moDto.BOMEffDate = DateTime.MaxValue;//BOMEffeDate  BOM有效日期  
+            //moDto.BOMType = 0;//BOM类型，0是自制，默认自制
+            //moDto.BOMVersion = new CBO.Pub.Controller.CommonArchiveDataDTOData();
+            moDto.BusinessPerson = new CommonArchiveDataDTOData
             {
                 ID = order.COperator.ID,
                 Code = order.COperator.Code,
                 Name = order.COperator.Name
             }; // 业务员  
             moDto.CompleteDate = order.DeliverDate;
-            //moDto.CostField
+        
             //moDto.CUD//暂时不知道是什么东西
-            moDto.DemandCode = 0;//需求分类
+            //moDto.DemandCode = 0;//需求分类
             //moDto.Department//生产部门
             moDto.DescFlexField = new UFIDA.U9.Base.FlexField.DescFlexField.DescFlexSegmentsData();
-            moDto.Description = "";
-            moDto.DocNo = "";
-            moDto.DocType = new UFIDA.U9.CBO.Pub.Controller.CommonArchiveDataDTOData
+            moDto.DocNo = order.MakReq;
+            moDto.DocType = new CommonArchiveDataDTOData
             {
                 Code = "",
                 Name = ""
@@ -90,24 +107,24 @@
             moDto.ExpandLevel = 2;
             //moDto.IsCapacityLimit
             //moDto.IsFirmed
-            //moDto.ItemMaster = new CBO.Pub.Controller.CommonArchiveDataDTOData
-            //{
-            //    Code = order.ItemID.Code,
-            //    Name = order.ItemID.Name
-            //};
+            moDto.ItemMaster = new CommonArchiveDataDTOData
+            {
+                Code = order.ItemID.Code,
+                Name = order.ItemID.Name
+            };
             //moDto.ItemVersion
             moDto.MOOperationDTOs = new List<MOOperationDTOData>();
             moDto.MOPickListDTOs = new List<MOPickListDTOData>();
             //moDto.MOSourceDocType
             moDto.MOSrcRelationDTOs = new List<MOSrcRelationDTOData>();
             moDto.MRPQty = (decimal)order.Qty;
-            moDto.Org = new CBO.Pub.Controller.CommonArchiveDataDTOData
+            moDto.Org = new CommonArchiveDataDTOData
             {
                 ID = Context.LoginOrg.ID,
                 Code = Context.LoginOrg.Code,
                 Name = Context.LoginOrg.Name
             };
-            moDto.OwnerOrg = new CBO.Pub.Controller.CommonArchiveDataDTOData
+            moDto.OwnerOrg = new CommonArchiveDataDTOData
             {
                 ID = order.Org
             };
@@ -116,7 +133,7 @@
             //moDto.ProductLotNo
             moDto.ProductQty = (decimal)order.Qty;
             //moDto.ProductUOM
-            moDto.Project = new UFIDA.U9.CBO.Pub.Controller.CommonArchiveDataDTOData
+            moDto.Project = new CommonArchiveDataDTOData
             {
                 Code = order.SCPOID.Code,//SCPOBE.SCPO
                 Name = order.SCPOID.Name
@@ -126,11 +143,12 @@
             //moDto.RoutingEffeDate
             //moDto.RoutingVer
             //moDto.SCVBin
-            //moDto.SCVWh=
+            moDto.SCVWh = new CommonArchiveDataDTOData { };
+            moDto.CostField = new CommonArchiveDataDTOData { };//根据入库仓库带出
             //moDto.Seiban
             moDto.SnList = new List<string>();
             moDto.StartDate = order.ReceiveDate;
-            moDto.Task = new UFIDA.U9.CBO.Pub.Controller.CommonArchiveDataDTOData
+            moDto.Task = new CommonArchiveDataDTOData
             {
                 Code = "",//SCPOBE.SCPO
                 Name = ""
