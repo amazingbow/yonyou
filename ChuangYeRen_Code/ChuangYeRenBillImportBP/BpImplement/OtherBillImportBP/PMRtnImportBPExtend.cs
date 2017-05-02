9c7b2946d74967e603dbb2b165568516890e9c3e
qingxuo0 <312315495@qq.com>
2017-05-02 11:23:55 +0800

    other

--- ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/PMRtnImportBPExtend.cs
+++ ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/PMRtnImportBPExtend.cs
@@ -1,45 +1,167 @@
 ﻿namespace UFIDA.U9.Cust.ChuangYeRenBillImportBP.OtherBillImportBP
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Text; 
-	using UFSoft.UBF.AopFrame;	
-	using UFSoft.UBF.Util.Context;
+    using PublicDataTransObj;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using UFIDA.U9.ISV.RCV.DTO;
+    using UFIDA.U9.ISV.RCV.Proxy;
+    using UFIDA.U9.CBO.Pub.Controller;
+    using UFSoft.UBF.AopFrame;
+    using UFSoft.UBF.Util.Context;
+    using UFSoft.UBF.PL.Engine;
+    using InvStockBE;
+    using UFIDA.U9.Base;
 
-	/// <summary>
-	/// PMRtnImportBP partial 
-	/// </summary>	
-	public partial class PMRtnImportBP 
-	{	
-		internal BaseStrategy Select()
-		{
-			return new PMRtnImportBPImpementStrategy();	
-		}		
-	}
-	
-	#region  implement strategy	
-	/// <summary>
-	/// Impement Implement
-	/// 
-	/// </summary>	
-	internal partial class PMRtnImportBPImpementStrategy : BaseStrategy
-	{
-		public PMRtnImportBPImpementStrategy() { }
+    /// <summary>
+    /// PMRtnImportBP partial 
+    /// </summary>	
+    public partial class PMRtnImportBP
+    {
+        internal BaseStrategy Select()
+        {
+            return new PMRtnImportBPImpementStrategy();
+        }
+    }
 
-		public override object Do(object obj)
-		{						
-			PMRtnImportBP bpObj = (PMRtnImportBP)obj;
-			
-			//get business operation context is as follows
-			//IContext context = ContextManager.Context	
-			
-			//auto generating code end,underside is user custom code
-			//and if you Implement replace this Exception Code...
-			throw new NotImplementedException();
-		}		
-	}
+    #region  implement strategy
+    /// <summary>
+    /// Impement Implement
+    /// 
+    /// </summary>	
+    internal partial class PMRtnImportBPImpementStrategy : BaseStrategy
+    {
+        public PMRtnImportBPImpementStrategy() { }
 
-	#endregion
-	
-	
+        public override object Do(object obj)
+        {
+            PMRtnImportBP bpObj = (PMRtnImportBP)obj;
+            PublicReturnDTO pub = new PublicReturnDTO();
+            pub.Flag = false;
+            pub.Message = "";
+            CreateRCVSRVProxy createProxy = new CreateRCVSRVProxy();
+            createProxy.RCVList = new List<OBAReceivementDTOData>();
+            try
+            {
+                OBAReceivementDTOData headDto = getInfo(bpObj.ID);
+                createProxy.RCVList.Add(headDto);
+                var data = createProxy.Do();
+                if (data.Count == 0) return pub;
+                pub.DocID = data[0].ID;
+                pub.DocNo = data[0].DocNo;             
+                ApproveRCVSRVProxy approveproxy = new ApproveRCVSRVProxy();
+                approveproxy.RCVLogicKeyINFOs = new List<RCVLogicKeyINFOData>();
+                RCVLogicKeyINFOData AppData = new RCVLogicKeyINFOData
+                {
+                    DocNo = pub.DocNo,
+                    Org=Context.LoginOrg.ID,
+                    RcvID=pub.DocID,
+                };
+                approveproxy.RCVLogicKeyINFOs.Add(AppData);
+                approveproxy.Do(Context.LoginOrg.ID);
+            }
+            catch (Exception ex)
+            {
+                pub.Flag = false;
+                pub.Message = ex.Message;
+                throw new Exception(ex.Message);
+            }
+
+            return pub;
+        }
+        private OBAReceivementDTOData getInfo(long stockID)
+        {
+            //头
+            //通过传入的StockID找到对应进出仓明细表数据
+            InvStock Invstock = InvStock.Finder.FindByID(stockID); //进出仓明细表头
+            ////为头信息赋值
+            InvStocks.EntityList<InvStocks> InvstocksList = Invstock.InvStocks; //进出仓明细表行表集合
+            ////为对应行信息赋值，添加到头对应行集合里面
+            OBAReceivementDTOData headDto = new OBAReceivementDTOData();
+            //headDto.RcvDocType = new 
+            headDto.RcvDocType = new PM.DTOs.BESimp4UIDTOData //单据类型
+            {
+                Name = Invstock.IOC.Name,
+            };
+            headDto.ReceivementType = 1; ////收货单类型 0:采购收货 1：采购退货 2：销售退回收货单
+            headDto.SrcDocType = 0;//来源单据类型 0：手工创建
+            headDto.Supplier = new CBO.SCM.Supplier.SupplierMISCInfoData  //供应商
+            {
+                Code = Invstock.SupplyID.Code,
+            };
+            //headDto.Payer = new CBO.SCM.Supplier.SupplierMISCInfoData //付款供应商
+            //{
+            //    Code = "XX01",
+            //};
+            headDto.IsInitEvaluation = false; //是否期初
+            headDto.BusinessDate = System.DateTime.Now; //业务制单时间
+            headDto.AC = new PM.DTOs.BESimp4UIDTOData   //核币
+            {
+
+                ID = Invstock.Currency.ID,
+            };
+            headDto.TC = new PM.DTOs.BESimp4UIDTOData  //币种
+            {
+                ID = Invstock.Currency.ID,
+            };
+            headDto.AccountOrg = new PM.DTOs.BESimp4UIDTOData //核算组织
+            {
+               ID = Invstock.OrgID,
+            };
+            headDto.Memo = Invstock.CNote;  //备注
+            //headDto.PostPeriod //记账期间
+            headDto.PaymentTerm = new PM.DTOs.BESimp4UIDTOData //付款条件
+            {
+                Code = "01",
+            };
+            headDto.ConfirmTerm = new PM.DTOs.BESimp4UIDTOData //立账条件
+            {
+                Code = "02",
+            };
+            headDto.FindPriceDate = System.DateTime.Now; //取价日期
+            headDto.IsPriceIncludeTax = true; //价格含税
+            headDto.SysState = ObjectState.Inserted;
+            List<OBARcvLineDTOData> LineDtolist = new List<OBARcvLineDTOData>();
+            foreach (InvStocks Invstocks in InvstocksList) //遍历进出仓明细表行表中所有的行信息
+            {
+                //    //Invstocks.属性 //               
+                OBARcvLineDTOData lineDto = new OBARcvLineDTOData();
+                lineDto.ItemInfo = new CBO.SCM.Item.ItemInfoData
+                {
+                    ItemCode = Invstocks.ItemID.Code,
+                };
+                lineDto.SysPriceTC = Invstocks.BalPrice; //系统价
+                lineDto.OrderPriceTC = Invstocks.BalPrice;  //定价
+                lineDto.FinallyPriceTC = Invstocks.BalPrice; //最终价
+                lineDto.RejectQtyTU = Invstocks.InQty-Invstocks.RcvQty; //拒收数量
+                lineDto.RtnFillQtyTU = Invstocks.RcvQty; //补退数量
+                //lineDto.RtnDeductQtyTU = 1; //退扣数量
+                lineDto.Wh = new PM.DTOs.BESimp4UIDTOData   //存储地点
+                {
+                    Code = Invstocks.StockID.Code,
+                };
+                //lineDto.RcvDept = new PM.DTOs.BESimp4UIDTOData //部门
+                //{
+                //    Code = Invstocks.
+                //};
+                //lineDto.WhMan = new PM.DTOs.BESimp4UIDTOData
+                //{
+                //    Code = "YZ0252"
+                //};
+                lineDto.SysState = ObjectState.Inserted;
+                lineDto.FreeReason = -1;
+                lineDto.FreeType = -1;
+                lineDto.RejectReason = -1;
+                LineDtolist.Add(lineDto);
+            }
+            //headDto.TaxRate = //税率
+            
+            headDto.RcvLines = LineDtolist;
+            return headDto;
+        }
+    }
+
+    #endregion
+
+
 }
--- ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/TransferApplyImportBPExtend.cs
+++ ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/TransferApplyImportBPExtend.cs
@@ -1,45 +1,157 @@
 ﻿namespace UFIDA.U9.Cust.ChuangYeRenBillImportBP.OtherBillImportBP
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Text; 
-	using UFSoft.UBF.AopFrame;	
-	using UFSoft.UBF.Util.Context;
+    using InvStockBE;
+    using PublicDataTransObj;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using UFIDA.U9.CBO.Pub.Controller;
+    using UFIDA.U9.ISV.TransferInISV;
+    using UFIDA.U9.ISV.TransferInISV.Proxy;
+    using UFSoft.UBF.AopFrame;
+    using UFSoft.UBF.PL.Engine;
+    using UFSoft.UBF.Util.Context;
 
-	/// <summary>
-	/// TransferApplyImportBP partial 
-	/// </summary>	
-	public partial class TransferApplyImportBP 
-	{	
-		internal BaseStrategy Select()
-		{
-			return new TransferApplyImportBPImpementStrategy();	
-		}		
-	}
-	
-	#region  implement strategy	
-	/// <summary>
-	/// Impement Implement
-	/// 
-	/// </summary>	
-	internal partial class TransferApplyImportBPImpementStrategy : BaseStrategy
-	{
-		public TransferApplyImportBPImpementStrategy() { }
+    /// <summary>
+    /// TransferApplyImportBP partial 
+    /// </summary>	
+    public partial class TransferApplyImportBP
+    {
+        internal BaseStrategy Select()
+        {
+            return new TransferApplyImportBPImpementStrategy();
+        }
+    }
 
-		public override object Do(object obj)
-		{						
-			TransferApplyImportBP bpObj = (TransferApplyImportBP)obj;
-			
-			//get business operation context is as follows
-			//IContext context = ContextManager.Context	
-			
-			//auto generating code end,underside is user custom code
-			//and if you Implement replace this Exception Code...
-			throw new NotImplementedException();
-		}		
-	}
+    #region  implement strategy
+    /// <summary>
+    /// Impement Implement
+    /// 
+    /// </summary>	
+    internal partial class TransferApplyImportBPImpementStrategy : BaseStrategy
+    {
+        public TransferApplyImportBPImpementStrategy() { }
 
-	#endregion
-	
-	
+        public override object Do(object obj)
+        {
+            TransferApplyImportBP bpObj = (TransferApplyImportBP)obj;
+            PublicReturnDTO pub = new PublicReturnDTO();
+            pub.Flag = false;
+            pub.Message = "";
+            
+
+            CommonCreateTransferInSVProxy createProxy = new CommonCreateTransferInSVProxy();
+            createProxy.TransferInDTOList = new List<ISV.TransferInISV.IC_TransferInDTOData>();
+            try
+            {
+                IC_TransferInDTOData ApplyDto = getInfo(bpObj.ID);
+                createProxy.TransferInDTOList.Add(ApplyDto);
+                var data = createProxy.Do();                
+                if (data.Count == 0) return pub;
+                pub.DocID = data[0].ID;
+                pub.DocNo = data[0].Code;                
+            }
+            catch (Exception ex)
+            {
+                pub.Flag = false;
+                pub.Message = ex.Message;
+                throw new Exception(ex.Message);
+            }
+            
+            return pub;
+        }
+
+        private IC_TransferInDTOData getInfo(long stockID)
+        {
+
+            //表头
+            IC_TransferInDTOData headDto = new IC_TransferInDTOData();
+            // InvStock Stock = InvStock.Finder.FindByID(stockID);
+            //InvStocks.EntityList<InvStocks> stocksList = Stock.InvStocks;
+
+            //headDto.CreatedBy //创建人
+            //headDto.CreatedOn     //创建时间
+            //headDto.DescFlexField //实体扩展字段
+            //headDto.ID //ID
+            //headDto.SOBAccountPeriod //记账期间
+            headDto.Org = new CommonArchiveDataDTOData //组织
+            {
+                Code = "201",
+            };
+            headDto.TransInDocType = new CommonArchiveDataDTOData   //单据类型
+            {
+                Code = "DR01",
+
+            };
+            headDto.ApprovedBy = "张三";  //审核人
+           // headDto.Status = 2; // 状态
+            headDto.TransferType = 0;       //调入类型
+            headDto.TransferDirection = 0;      //调拨方向
+            headDto.BusinessDate = System.DateTime.Now; //日期
+            headDto.Memo = ".net调用测试";    //备注
+            //headDto.DocNo = "Text00001";
+            headDto.SysState = ObjectState.Inserted;
+            //表行
+            List<IC_TransInLineDTOData> LineList = new List<IC_TransInLineDTOData>();
+            //foreach (InvStocks stocks in stocksList)
+            //{
+            IC_TransInLineDTOData lineDto = new IC_TransInLineDTOData();
+            //lineDto.DocLineNo  //行号 
+            //lineDto.Project     //项目
+            //lineDto.StoreUOM      //调入库存单位
+            lineDto.StoreUOMQty = 1;  //调入库存数量
+            //lineDto.CostUOM   //成本单位
+            lineDto.CostPrice = 100; //单价
+            lineDto.CostUOMQty = 1;
+            //lineDto.LotInfo //批号
+            //lineDto.CostCurrency   //币种
+            lineDto.CostMoney = (lineDto.CostPrice * lineDto.CostUOMQty); //成本
+            //lineDto.IsVMI     //IsVMI标志
+            lineDto.TransInSuppInfo = new CBO.SCM.Supplier.SupplierMISCInfoData();
+            lineDto.TransInSuppInfo.Code = "006"; //供应商       
+            lineDto.ItemInfo = new CBO.SCM.Item.ItemInfoData();
+            lineDto.ItemInfo.ItemCode = "320060023";   //料品信息
+            lineDto.TransInWh = new CommonArchiveDataDTOData //库位
+            {
+                Code = "20118",
+            };
+            lineDto.ZeroCost = true;  //零成本
+            lineDto.SysState = ObjectState.Inserted;
+
+            //子表
+            List<IC_TransInSubLineDTOData> SubLineList = new List<IC_TransInSubLineDTOData>();
+            IC_TransInSubLineDTOData SubLine = new IC_TransInSubLineDTOData();
+            SubLine.TransOutOrg = new CommonArchiveDataDTOData  //调出组织
+            {
+                Code = "201",
+            };
+            SubLine.TransOutWh = new CommonArchiveDataDTOData //库存地址
+            {
+                Code = "20101",
+            };
+            //SubLine.TransOutSuppInfo  //供应商
+            //SubLine.TransOutCustInfo //客户
+            //SubLine.TransInSUQty   //调入数量
+            //SubLine.DocLineNo  //子行号
+            //SubLine.StoreUOM  //调出单位
+            //SubLine.StoreUOMQty  //调出数量
+            //SubLine.CostPrice //单价
+            //SubLine.CostMoney  //成本
+            //SubLine.ZeroCost   //零成本
+            SubLine.LotInfo = new CBO.SCM.PropertyTypes.LotInfoData();
+            //SubLine.LotInfo   //有效性
+            //SubLine.TransOutDept      //部门
+            SubLine.SysState = ObjectState.Inserted;
+            SubLineList.Add(SubLine);
+            lineDto.TransInSubLines = SubLineList;
+            LineList.Add(lineDto);
+            //}
+            headDto.TransInLines = LineList;
+            return headDto;
+        }
+    }
+
+    #endregion
+
+
 }
