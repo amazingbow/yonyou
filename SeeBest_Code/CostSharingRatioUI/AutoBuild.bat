7b3d543e96fd8a6dfaca8e533e5af08893c4b364
Amazingbow <wodemanur@163.com>
2017-05-23 16:39:37 +0800

    视贝

---
+++ SeeBest_Code/CostSharingRatioUI/AutoBuild.bat
@@ -0,0 +1,17 @@
+﻿
+echo reset IIS
+echo iisreset
+
+echo beging copy UI dll to portal
+
+copy .\bin\Debug\CostSharingRatioUI.WebPart.dll  D:\yonyou\U9V50\Portal\UILib
+copy .\bin\Debug\CostSharingRatioUI.WebPart.pdb  D:\yonyou\U9V50\Portal\UILib
+
+echo begin run build UI Script
+echo 目录：.\..\..\..\..\yonyou\UBFV50\U9.VOB.Product.Other\\u_ui\UIScript\
+
+echo .\..\..\..\..\yonyou\UBFV50\U9.VOB.Product.UBF\UBFStudio\Runtime\\..\DBScriptExecutor.exe -connStr "packet size=4096;user id=sa;Connection Timeout=150;Max Pool size=1500;data source=localhost;persist security info=True;initial catalog=GXU9DB;password=admin123" -NotDropDB -NotWriteLog -ExecuteDelete ..\..\U9.VOB.Product.Other\u_ui\UIScript\
+
+echo ui buid end
+pause
+
---
+++ SeeBest_Code/SeeBestAdvertisementBP/AutoBuild.bat
@@ -0,0 +1,20 @@
+﻿
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.Deploy.dll  D:\yonyou\U9V50\Portal\ApplicationLib
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.Deploy.pdb  D:\yonyou\U9V50\Portal\ApplicationLib
+copy .\BpAgent\bin\Debug\SeeBestAdvertisementBP.Agent.dll  D:\yonyou\U9V50\Portal\ApplicationLib
+copy .\BpAgent\bin\Debug\SeeBestAdvertisementBP.Agent.pdb  D:\yonyou\U9V50\Portal\ApplicationLib
+
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.Deploy.dll  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.Deploy.pdb  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpAgent\bin\Debug\SeeBestAdvertisementBP.Agent.dll  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpAgent\bin\Debug\SeeBestAdvertisementBP.Agent.pdb  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.dll  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.pdb  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+copy .\BpImplement\bin\Debug\SeeBestAdvertisementBP.ubfsvc  D:\yonyou\U9V50\Portal\ApplicationServer\Libs
+
+
+
+echo 请手工将该bat文件打开，将下面这段内容与D:\yonyou\U9V50\Portal\RestServices\web.config进行合并。
+
+
+pause
