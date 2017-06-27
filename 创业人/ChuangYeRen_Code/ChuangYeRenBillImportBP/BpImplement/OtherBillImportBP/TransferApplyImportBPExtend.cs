7724f24c3a9efa5eb19dc1e35aabc18dbb8771d4
qingxuo0 <312315495@qq.com>
2017-06-27 17:30:35 +0800

    创业人调入单接口代码 金龙凭证根据科目编码内容回写扩展字段代码

--- 创业人/ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/TransferApplyImportBPExtend.cs
+++ 创业人/ChuangYeRen_Code/ChuangYeRenBillImportBP/BpImplement/OtherBillImportBP/TransferApplyImportBPExtend.cs
@@ -81,23 +81,24 @@
             #region  表头信息(含表行、子行)
             IC_TransferInDTOData headDto = new IC_TransferInDTOData();
             //入库单头信息inStock
-            InvStock inStock = InvStock.Finder.FindByID(InSourceID);
-            headDto.CreatedBy = inStock.CreatedBy;
-            headDto.CreatedOn = inStock.CreatedOn;
-            headDto.CreatedOn = inStock.CreatedOn;    //创建时间
+            //InvStock inStock = InvStock.Finder.FindByID(InSourceID);
+            headDto.CreatedBy = Context.LoginUser;//inStock.CreatedBy;
+            headDto.CreatedOn = System.DateTime.Now;//inStock.CreatedOn;
+            //headDto.CreatedOn = //inStock.CreatedOn;    //创建时间
             headDto.Org = new CommonArchiveDataDTOData //组织
             {
-                ID = inStock.OrgID,
+                ID = 1001504150120242, //inStock.OrgID,
             };
             headDto.TransInDocType = new CommonArchiveDataDTOData   //单据类型
             {
-                Name = inStock.IOC.Name,
+                Code = "DR02"
+                //Name = inStock.IOC.Name,
             };
             headDto.TransferType = 0;       //调入类型
             headDto.TransferDirection = 0;      //调拨方向
             headDto.BusinessDate = System.DateTime.Now; //日期
-            headDto.Memo = inStock.CNote;    //备注
-            headDto.DocNo = inStock.BillNO;//单据编号
+            headDto.Memo = "测试";//inStock.CNote;    //备注
+            //headDto.DocNo = inStock.BillNO;//单据编号
             #region 表头不需传值部分
             //headDto.DescFlexField //实体扩展字段
             //headDto.ID //ID
@@ -105,101 +106,127 @@
             //headDto.ApprovedBy = Stock.;  //审核人
             // headDto.Status = 2; // 状态
             #endregion
-            headDto.SysState = ObjectState.Inserted;
+
             #region 表行含子行
             List<IC_TransInLineDTOData> LineList = new List<IC_TransInLineDTOData>();
             //入库单行信息
-            InvStocks.EntityList<InvStocks> stocksList = inStock.InvStocks;
-            foreach (InvStocks stocks in stocksList)
+            //InvStocks.EntityList<InvStocks> stocksList = inStock.InvStocks;
+            //foreach (InvStocks stocks in stocksList)
+            //{
+            IC_TransInLineDTOData lineDto = new IC_TransInLineDTOData();
+            //lineDto.DocLineNo=  //行号 
+            //lineDto.Project=stocks.     //项目
+
+            //lineDto.StoreUOM = new CommonArchiveDataDTOData
+            //{
+            //    ID = stocks.ItemID.UOM.ID,
+            //};    //调入库存单位
+            lineDto.StoreUOMQty = 1; //stocks.InQty;  //调入库存数量
+
+            //lineDto.LotInfo = new CBO.SCM.PropertyTypes.LotInfoData
+            //{
+            //    LotCode = stocks.BarCode,
+            //}; //批号
+            lineDto.CostCurrency = new CommonArchiveDataDTOData
             {
-                IC_TransInLineDTOData lineDto = new IC_TransInLineDTOData();
-                //lineDto.DocLineNo=  //行号 
-                //lineDto.Project=stocks.     //项目
+                ID = 1,
+                //ID = stocks.Currency.ID,
+            };  //币种
+            //lineDto.CostPrice = 1; //单价
+            //lineDto.CostUOMQty = 1;
+            //lineDto.CostMoney = (lineDto.CostPrice * lineDto.CostUOMQty); //成本
 
-                lineDto.StoreUOM = new CommonArchiveDataDTOData
-                {
-                    ID = stocks.ItemID.UOM.ID,
-                };    //调入库存单位
-                lineDto.StoreUOMQty = stocks.InQty;  //调入库存数量
+            // lineDto.TransInSuppInfo = new CBO.SCM.Supplier.SupplierMISCInfoData();
 
-                lineDto.LotInfo = new CBO.SCM.PropertyTypes.LotInfoData
-                {
-                    LotCode = stocks.BarCode,
-                }; //批号
-                lineDto.CostCurrency = new CommonArchiveDataDTOData
-                {
-                    ID = stocks.Currency.ID,
-                };  //币种
-                lineDto.CostMoney = (lineDto.CostPrice * lineDto.CostUOMQty); //成本
+            lineDto.ItemInfo = new CBO.SCM.Item.ItemInfoData();
+            lineDto.ItemInfo.ItemCode = "301020001";//stocks.ItemID.Code;
+            lineDto.StorageType = 4;
+            //UFIDA.U9.CBO.SCM.ProjectTask.Project Project = UFIDA.U9.CBO.SCM.ProjectTask.Project.Finder.Find("Org ='" + 组织编码 + "' and Code='" + 项目编码 + "'");
+            UFIDA.U9.CBO.SCM.ProjectTask.Project Project = UFIDA.U9.CBO.SCM.ProjectTask.Project.Finder.Find("Org =" + 组织ID + " and Code='" + 项目编码 + "'");
+            long ProjectId =Project.ID;
+            lineDto.Project = new CommonArchiveDataDTOData();
+            lineDto.Project.ID = 1001508120038268;
+            lineDto.TransInWh = new CommonArchiveDataDTOData //库位
+            {
+                Code = "20201",
+                // ID = stocks.StockID.ID,
+            };
+            // lineDto.ZeroCost = true;  //零成本
+            #region 表行不需传值部分
+            //lineDto.CostUOM   //成本单位
 
-                lineDto.TransInSuppInfo = new CBO.SCM.Supplier.SupplierMISCInfoData();
+            //lineDto.CostUOMQty = stocks.InQty;
+            //lineDto.IsVMI     //IsVMI标志
+            //lineDto.TransInSuppInfo.Code = "006";
+            //lineDto.TransInSuppInfo.Code = "006"; //供应商       
+            //lineDto.ItemInfo.ItemCode = "320060023";   //料品信息
+            #endregion
 
-                lineDto.ItemInfo = new CBO.SCM.Item.ItemInfoData();
-                lineDto.ItemInfo.ItemCode = stocks.ItemID.Code;
+            #region  子行信息
 
-                lineDto.TransInWh = new CommonArchiveDataDTOData //库位
-                {
-                    ID = stocks.StockID.ID,
-                };
-                lineDto.ZeroCost = true;  //零成本
-                #region 表行不需传值部分
-                //lineDto.CostUOM   //成本单位
-                //lineDto.CostPrice = stocks.Price; //单价
-                //lineDto.CostUOMQty = stocks.InQty;
-                //lineDto.IsVMI     //IsVMI标志
-                //lineDto.TransInSuppInfo.Code = "006";
-                //lineDto.TransInSuppInfo.Code = "006"; //供应商       
-                //lineDto.ItemInfo.ItemCode = "320060023";   //料品信息
-                #endregion
-                lineDto.SysState = ObjectState.Inserted;
-                #region  子行信息
+            //出库单信息
+            //InvStock outStock = InvStock.Finder.FindByID(OutSourceID);
+            //InvStocks.EntityList outInvStocksList = outStock.InvStocks;
+            //子行信息
+            List<IC_TransInSubLineDTOData> SubLineList = new List<IC_TransInSubLineDTOData>();
+            IC_TransInSubLineDTOData SubLine = new IC_TransInSubLineDTOData();
+            //foreach (var outInvStocks in outInvStocksList)
+            //{
+            SubLine.TransOutOrg = new CommonArchiveDataDTOData
+            {
+                ID = 1001504150120017,
+            };
+            //if (outInvStocks.Item != outInvStocks.Item) continue;
+            SubLine.TransOutWh = new CommonArchiveDataDTOData //库存地址
+            {
+                Code = "20101",//outInvStocks.StockID.ID,
+            };
+            SubLine.StoreUOMQty = 1;//outInvStocks.InQty;  //调出数量
+            SubLine.StorageType = 4;
+            SubLine.TransOutCustInfo = new CBO.SCM.Customer.CustomerMISCInfoData
+            {
+                Customer = 1001505050112514,//客户ID
+                //Code = "",  //客户Code
+                //Name = "", //客户名称                
+            };
+            //SubLine.Project = new CommonArchiveDataDTOData
+            //{
+            //    ID = 1001508120038459,
+            //    SysState = ObjectState.NeedLoad,
+            //};
+            //SubLine.LotInfo = new CBO.SCM.PropertyTypes.LotInfoData()
+            //{
+            //    LotCode = outInvStocks.BarCode,
+            //};
+            #region 子行不需传值部分
+            //        SubLine.TransOutOrg = new CommonArchiveDataDTOData  //调出组织
+            //{
+            //    ID = outInvStocks.,
+            //};
+            //SubLine.DocLineNo=outStock.
+            //SubLine.TransOutSuppInfo  //供应商
+            //SubLine.TransOutCustInfo //客户
+            //SubLine.TransInSUQty   //调入数量
+            //SubLine.DocLineNo  //子行号
+            //SubLine.StoreUOM  //调出单位
+            //SubLine.CostPrice //单价
+            //SubLine.CostMoney  //成本
+            //SubLine.ZeroCost   //零成本
+            //SubLine.LotInfo   //有效性
+            //SubLine.TransOutDept      //部门
+            #endregion
+            SubLine.SysState = ObjectState.Inserted;
+            SubLineList.Add(SubLine);
 
-                //出库单信息
-                InvStock outStock = InvStock.Finder.FindByID(OutSourceID);
-                InvStocks.EntityList outInvStocksList = outStock.InvStocks;
-                //子行信息
-                List<IC_TransInSubLineDTOData> SubLineList = new List<IC_TransInSubLineDTOData>();
-                IC_TransInSubLineDTOData SubLine = new IC_TransInSubLineDTOData();
-                foreach (var outInvStocks in outInvStocksList)
-                {
-                    if (outInvStocks.Item != outInvStocks.Item) continue;
-                    SubLine.TransOutWh = new CommonArchiveDataDTOData //库存地址
-                    {
-                        ID = outInvStocks.StockID.ID,
-                    };
-                    SubLine.StoreUOMQty = outInvStocks.InQty;  //调出数量
+            //   }
+            lineDto.TransInSubLines = SubLineList;
+            #endregion
+            lineDto.SysState = ObjectState.Inserted;
+            LineList.Add(lineDto);
+            headDto.TransInLines = LineList;
+            headDto.SysState = ObjectState.Inserted;
 
-                    SubLine.LotInfo = new CBO.SCM.PropertyTypes.LotInfoData()
-                    {
-                        LotCode = outInvStocks.BarCode,
-                    };
-                    #region 子行不需传值部分
-                    //        SubLine.TransOutOrg = new CommonArchiveDataDTOData  //调出组织
-                    //{
-                    //    ID = outInvStocks.,
-                    //};
-                    //SubLine.DocLineNo=outStock.
-                    //SubLine.TransOutSuppInfo  //供应商
-                    //SubLine.TransOutCustInfo //客户
-                    //SubLine.TransInSUQty   //调入数量
-                    //SubLine.DocLineNo  //子行号
-                    //SubLine.StoreUOM  //调出单位
-                    //SubLine.CostPrice //单价
-                    //SubLine.CostMoney  //成本
-                    //SubLine.ZeroCost   //零成本
-                    //SubLine.LotInfo   //有效性
-                    //SubLine.TransOutDept      //部门
-                    #endregion
-                    SubLine.SysState = ObjectState.Inserted;
-                    SubLineList.Add(SubLine);
-
-                }
-                lineDto.TransInSubLines = SubLineList;
-                #endregion
-                LineList.Add(lineDto);
-                headDto.TransInLines = LineList;
-
-            }
+            // }
             #endregion
 
             return headDto;
