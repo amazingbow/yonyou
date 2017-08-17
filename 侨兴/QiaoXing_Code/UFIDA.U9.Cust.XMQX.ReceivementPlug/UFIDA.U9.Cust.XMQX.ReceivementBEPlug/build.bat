ca7d0a12f99703d37d92a55766b6b5d7ca3b797d
qingxuo0 <312315495@qq.com>
2017-08-17 15:11:50 +0800

    侨兴

---
+++ 侨兴/QiaoXing_Code/UFIDA.U9.Cust.XMQX.ReceivementPlug/UFIDA.U9.Cust.XMQX.ReceivementBEPlug/build.bat
@@ -0,0 +1,5 @@
+@echo off
+echo build start...
+if exist "%~dp0build.log" (del /Q "%~dp0build.log")
+%windir%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe "C:\Users\Administrator\Desktop\XMJL\Code\UFIDA.U9.Cust.XMQX.ReceivementPlug\UFIDA.U9.Cust.XMQX.ReceivementBEPlug\UFIDA.U9.Cust.XMQX.ReceivementBEPlug.csproj" /t:Clean;Rebuild /p:WarningLevel=0;Configuration=Release 
+echo build end
